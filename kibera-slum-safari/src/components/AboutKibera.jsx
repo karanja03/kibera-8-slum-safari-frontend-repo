43dddf8f9777bcc4bf6146ad79a3024494bcf3d9
karanja03 <wambuik03@gmail.com>
2023-11-03 01:44:15 +0300

    Added About Page

--- kibera-slum-safari/src/components/AboutKibera.jsx
+++ kibera-slum-safari/src/components/AboutKibera.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import HeaderAK from "./HeaderAK";
+import IntroductionAk from "./IntroductionAK";
+import HistoryAK from "./HistoryAK";
+import PhotoGalleryAK from "./PhotoGalleryAK";
+import Population from "./Population";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Challenges from "./Challenges";
 
 
 function AboutKibera(){
     return(
         <div>
             <HeaderAK/>
+            <IntroductionAk/>
+            <div className="mt-12"> <HistoryAK/></div>
+            <PhotoGalleryAK/>
+            <Population/>
+            <Challenges/>
+            {/* <Maps/> */}
+           
             
         </div>
     )
 }
 
 export default AboutKibera
+
--- kibera-slum-safari/src/components/HeaderAK.jsx
+++ kibera-slum-safari/src/components/HeaderAK.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function HeaderAK(){
+  const navigate= useNavigate()
+  const handleaboutclick=()=>{
+    navigate('/introduction')
+  }
     return(
         <div>
-            <div className="h-screen p-8">
+            <div className=" p-8">
       <header className="text-center bg-custom  py-8">
         <h1 className="text-4xl font-bold mb-4">Exploring Kibera: A Glimpse into Nairobi's Largest Slum</h1>
       </header>
 
       <nav className="text-center my-8 bg-custom py-6 ">
         <ul className="space-x-10">
-          <li className="inline-block font-semibold">
-            <a href="#about">About Kibera</a>
+          <li className="inline-block font-semibold" onClick={handleaboutclick}>
+            About Kibera
           </li>
           <li className="inline-block font-semibold">
             <a href="#community">Community</a>
